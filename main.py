4db53c66051e31aa4eb66e95c0698eea197dc502
Severino Carlos <63283908+severinocarlos@users.noreply.github.com>
2021-08-17 15:07:32 -0300

    pontuação (check)

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import pygame as pg
 
-pg.init()
-
 ##########################
 # --> FUNÇÃO PARA RODAR O JOGO
 ##########################
 
-# --> INICIALIZAÇÃO DA TELA /NOME DA JANELA(GAME)/ CARREGAR A IMAGEM DO MAPA/ SETAR O ICONE DO JOGO NA ABA
-display = pg.display.set_mode((1080, 720))
+# --> INICIALIZAÇÃO
+pg.init()
+
+# --> INICIALIZAÇÃO DA TELA /NOME DA JANELA(GAME)/ CARREGAR A IMAGEM DO map/ SETAR O ICONE DO JOGO NA ABA
+display = pg.display.set_mode((800,600))
 pg.display.set_caption("Gotta catch 'em all!")
-mapa = pg.image.load("images/background.png")
-mapa = pg.transform.scale(mapa, (1080, 720))
+map = pg.image.load("images/background.png")
+map = pg.transform.scale(map, (800, 600))
+icon = pg.image.load("images/icon.png")
+pg.display.set_icon(icon)
 
 
-# icon = pg.image.load("images/icon.png")
-# pg.display.set_icon(icon)
-
+icon_poke = pg.image.load("images/pokeball.png")
 
 # --> INICIALIZAÇÃO E O CARRREGAMENTO DA MÚSICA DE FUNDO  ((PS: TÁ DANDO UNS BUGS VOU ARRUMAR DPS BY: SEVE))
-# pg.mixer.init()
-# pg.mixer.music.load("/home/juninho/Documentos/PROJETO-IP/fundo.mp3")
-# pg.mixer.music.play(-1)
+#pg.mixer.init()
+#pg.mixer.music.load("sounds/fundo.mp3")
+#pg.mixer.music.play(-1)
 
+# --> PONTUAÇÃO DO JOGO
+score_value = 0
+font = pg.font.Font("freesansbold.ttf", 32)
+text_position_x = 10
+text_position_y = 10
 
 def main():
-    # --> LOOP INFINITO QUE RODA O GAME
+    # --> LOOP INFINITO QUE RODA O GAME 
     while True:
-        # VERIFICAÇÃO PARA O FECHAMENTO DA JANELA
+        #VERIFICAÇÃO PARA O FECHAMENTO DA JANELA
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
@@ -34,11 +40,13 @@ def main():
                 if event.key == pg.K_ESCAPE:
                     pg.quit()   
 
-        display.fill((0, 0, 0))
-        display.blit(mapa, (0, 0))
-
-        # ATUALIZA O DISPLAY
+        
+        score = font.render("PONTUAÇÃO: " +str(score_value), True,  (255,255,255)) 
+        display.fill((0,0,0))
+        display.blit(map, (0,0))
+        display.blit(score, (10,10))
+        #display.blit(icon_poke, (10,10))
+        #ATUALIZA O DISPLAY
         pg.display.update()
-
-
+           
 main()
